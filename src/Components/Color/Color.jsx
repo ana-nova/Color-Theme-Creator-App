178849fd77c652b576c0a706e6785ce910ee1229
ana-nova <yu.ana@gmx.de>
2024-10-09 15:06:12 +0200

    edit color

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Color.css";
+import ColorForm from "../ColorForm/ColorForm";
 
-export default function Color({ color, onDeleteColor }) {
+export default function Color({ color, onDeleteColor, onUpdateColor }) {
   function handleDelete() {
     // Show a confirmation dialog before deleting
     const userConfirmed = window.confirm(
@@ -12,6 +14,17 @@ export default function Color({ color, onDeleteColor }) {
     }
   }
 
+  const [edit, setEdit] = useState(false);
+
+  function handleEdit() {
+    setEdit(!edit);
+  }
+
+  function handleUpdateColor(updatedColor) {
+    onUpdateColor(color.id, updatedColor); // Update the color in the main list
+    setEdit(false); // Exit edit mode after updating
+  }
+
   return (
     <div
       className="color-card"
@@ -20,10 +33,25 @@ export default function Color({ color, onDeleteColor }) {
         color: color.contrastText,
       }}
     >
-      <h3 className="color-card-headlight">{color.role}</h3>
-      <p>hex: {color.hex}</p>
-      <p>contrast: {color.contrastText}</p>
-      <button onClick={handleDelete}>delete</button>
+      {edit ? (
+        <ColorForm onSubmitColor={handleUpdateColor} initialData={color}/>
+      ) : (
+      <>
+        <h3 className="color-card-headlight">{color.role}</h3>
+        <p>hex: {color.hex}</p>
+        <p>contrast: {color.contrastText}</p>
+        <button onClick={handleDelete}>delete</button>
+        <button onClick={handleEdit}>edit</button>
+      </>
+      )}
     </div>
   );
 }
+
+
+/*
+    The ) : ( syntax is part of a ternary operator that helps you choose between 
+    two expressions based on a condition. It's a concise way to write simple 
+    conditional logic in your code.
+
+*/
